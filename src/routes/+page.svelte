275cc7279a29dbf30928bdd983261f8f580a6bcc
Projet 360 <37069190+Projet-360@users.noreply.github.com>
2024-08-12 15:47:00 +0200

    Setup DONE

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,26 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<script lang="ts">
+	import * as Table from '$lib/components/ui/table';
+
+	let { data } = $props();
+</script>
+
+<div class="mx-auto mt-8 max-w-3xl px-4 sm:px-6 lg:px-8">
+	<div class="space-y-6 rounded-md border p-4">
+		<Table.Root>
+			<Table.Header>
+				<Table.Row>
+					<Table.Head class="w-[100px]">ID</Table.Head>
+					<Table.Head>Name</Table.Head>
+				</Table.Row>
+			</Table.Header>
+			<Table.Body>
+				{#each data.users as user (user.id)}
+					<Table.Row>
+						<Table.Cell class="font-medium">{user.id}</Table.Cell>
+						<Table.Cell>{user.name}</Table.Cell>
+					</Table.Row>
+				{/each}
+			</Table.Body>
+		</Table.Root>
+	</div>
+</div>
