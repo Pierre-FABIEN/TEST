b6c25d39c6fa435e37d053ffa48f2d47db2c7e0a
Projet 360 <37069190+Projet-360@users.noreply.github.com>
2024-08-13 13:15:13 +0200

    Change url and add menu

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,4 +1,5 @@
 <script>
+	import { page } from '$app/stores';
 	import { ModeWatcher } from 'mode-watcher';
 	import '../app.css';
 
@@ -7,4 +8,25 @@
 
 <ModeWatcher />
 
-{@render children()}
+<div class="flex justify-center mt-5">
+	<nav class="flex items-center border rounded space-x-4 lg:space-x-6 rcb w-[500px] p-3">
+		<a
+			href="/"
+			class={`text-sm font-medium ${$page.url.pathname === '/dashboard' ? 'text-primary' : 'text-muted-foreground'} transition-colors hover:text-primary`}
+		>
+			Accueil
+		</a>
+		<a
+			href="/user"
+			class={`text-sm font-medium ${$page.url.pathname === '/dashboard/users' ? 'text-primary' : 'text-muted-foreground'} transition-colors hover:text-primary`}
+		>
+			Utilisateurs
+		</a>
+	</nav>
+</div>
+
+<main>
+	{@render children()}
+</main>
+
+
